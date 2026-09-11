5cd86d16ef065e1ba1436a8faa2e6bb7c031dc84
Xin898 <37047545+Xin898@users.noreply.github.com>
2026-09-11 23:39:32 +0200

    epic: add src/main/java/com/xin898/cryptrader/CrypTraderApplication.java

---
+++ src/main/java/com/xin898/cryptrader/CrypTraderApplication.java
@@ -0,0 +1,11 @@
+package com.xin898.cryptrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrypTraderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrypTraderApplication.class, args);
+    }
+}
